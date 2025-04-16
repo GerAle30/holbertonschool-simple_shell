ef47544b0e40045b7f65b31d9ae01bddfe896aff
Gerdia0117 <Gerald.froz@gmail.com>
2025-04-16 13:56:08 -0400

    fixed the spacings in the file shell.c

--- shell.c
+++ shell.c
@@ -8,40 +8,40 @@
  */
 int main(int argc, char **argv)
 {
-    char input[MAX_INPUT];
-    char **args;
-    ssize_t read_size;
-    int status = 1;
+	char input[MAX_INPUT];
+	char **args;
+	ssize_t read_size;
+	int status = 1;
 
-    (void)argc;
+	(void)argc;
 
-    while (status)
-    {
-        display_prompt();
+	while (status)
+	{
+		display_prompt();
 
-        read_size = read(STDIN_FILENO, input, MAX_INPUT);
-        if (read_size == -1)
-        {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
-        else if (read_size == 0)
-        {
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
+		read_size = read(STDIN_FILENO, input, MAX_INPUT);
+		if (read_size == -1)
+		{
+			perror("read");
+			exit(EXIT_FAILURE);
+		}
+		else if (read_size == 0)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
 
-        input[read_size] = '\0';
-        if (_strlen(input) == 0)
-            continue;
+		input[read_size] = '\0';
+		if (_strlen(input) == 0)
+			continue;
 
-        args = parse_input(input);
-        status = execute_command(args, argv[0]);
-        free(args);
-    }
+		args = parse_input(input);
+		status = execute_command(args, argv[0]);
+		free(args);
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
 /**
@@ -49,9 +49,9 @@ int main(int argc, char **argv)
  */
 void display_prompt(void)
 {
-    if (isatty(STDIN_FILENO))
-        printf(PROMPT);
-    fflush(stdout);
+	if (isatty(STDIN_FILENO))
+		printf(PROMPT);
+	fflush(stdout);
 }
 
 /**
@@ -61,47 +61,47 @@ void display_prompt(void)
  */
 char *find_path(const char *command)
 {
-    char *path, *dir, *full_path;
-    struct stat st;
+	char *path, *dir, *full_path;
+	struct stat st;
 
-    if (!command)
-        return NULL;
+	if (!command)
+		return NULL;
 
-    /* Check if command has path (/, ./) */
-    if (command[0] == '/' || command[0] == '.')
-    {
-        if (stat(command, &st) == 0)
-            return _strdup(command);
-        return NULL;
-    }
+	/* Check if command has path (/, ./) */
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (stat(command, &st) == 0)
+			return _strdup(command);
+		return NULL;
+	}
 
-    path = getenv("PATH");
-    if (!path)
-        return NULL;
+	path = getenv("PATH");
+	if (!path)
+		return NULL;
 
-    path = _strdup(path);
-    dir = strtok(path, ":");
-    full_path = malloc(MAX_PATH_LEN);
+	path = _strdup(path);
+	dir = strtok(path, ":");
+	full_path = malloc(MAX_PATH_LEN);
 
-    while (dir)
-    {
-        _strcat(full_path, dir);
-        _strcat(full_path, "/");
-        _strcat(full_path, command);
+	while (dir)
+	{
+		_strcat(full_path, dir);
+		_strcat(full_path, "/");
+		_strcat(full_path, command);
 
-        if (stat(full_path, &st) == 0)
-        {
-            free(path);
-            return full_path;
-        }
+		if (stat(full_path, &st) == 0)
+		{
+			free(path);
+			return full_path;
+		}
 
-        full_path[0] = '\0';
-        dir = strtok(NULL, ":");
-    }
+		full_path[0] = '\0';
+		dir = strtok(NULL, ":");
+	}
 
-    free(full_path);
-    free(path);
-    return NULL;
+	free(full_path);
+	free(path);
+	return NULL;
 }
 
 /**
@@ -112,48 +112,48 @@ char *find_path(const char *command)
  */
 int execute_command(char **args, char *shell_name)
 {
-    pid_t pid;
-    char *path;
-    int status;
+	pid_t pid;
+	char *path;
+	int status;
 
-    if (!args || !args[0])
-        return 1;
+	if (!args || !args[0])
+		return 1;
 
-    /* Handle built-in commands */
-    if (handle_builtins(args) == 0)
-        return 0;
+	/* Handle built-in commands */
+	if (handle_builtins(args) == 0)
+		return 0;
 
-    path = find_path(args[0]);
-    if (!path)
-    {
-        fprintf(stderr, "%s: 1: %s: not found\n", shell_name, args[0]);
-        return 1;
-    }
+	path = find_path(args[0]);
+	if (!path)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n", shell_name, args[0]);
+		return 1;
+	}
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        free(path);
-        return 1;
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(path);
+		return 1;
+	}
 
-    if (pid == 0)
-    {
-        if (execve(path, args, environ) == -1)
-        {
-            perror(shell_name);
-            free(path);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-        free(path);
-    }
+	if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			perror(shell_name);
+			free(path);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		free(path);
+	}
 
-    return 1;
+	return 1;
 }
 
 /**
@@ -163,16 +163,16 @@ int execute_command(char **args, char *shell_name)
  */
 int handle_builtins(char **args)
 {
-    if (_strcmp(args[0], "exit") == 0)
-        return 0;
+	if (_strcmp(args[0], "exit") == 0)
+		return 0;
 
-    if (_strcmp(args[0], "env") == 0)
-    {
-        char **env = environ;
-        while (*env)
-            printf("%s\n", *env++);
-        return 1;
-    }
+	if (_strcmp(args[0], "env") == 0)
+	{
+		char **env = environ;
+		while (*env)
+			printf("%s\n", *env++);
+		return 1;
+	}
 
-    return -1;
+	return -1;
 }
