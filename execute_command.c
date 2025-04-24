1d98d34d8dd802400afe4f0bcc4f40ca0b4d2169
GerAle30 <garciaslj@gmail.com>
2025-04-24 00:37:47 -0400

    mensaje

--- execute_command.c
+++ execute_command.c
@@ -1,49 +1,49 @@
 #include "shell.h"
 
 /**
- * execute_command - Creates a child process to execute a command
- * @args: NULL-terminated array of arguments
- * @program_name: Name of the shell program (for error messages)
+ * execute_command - Executes a command with fork and execve
+ * @args: Command and arguments
+ * @prog: Program name (for error messages)
  */
-void execute_command(char **args, char *program_name)
+void execute_command(char **args, char *prog)
 {
+	char *cmd = NULL, *p, *pcopy, *tok, path[PATH_MAX];
 	pid_t pid;
 	int status;
-	char buffer[PATH_MAX];
 
-	if (access(args[0], X_OK) == -1)
-	{
-		/* Try to resolve using PATH manually (no getenv) */
-		char *path = "/bin:/usr/bin:/usr/local/bin";
-		char *token = strtok(path, ":");
-
-		while (token)
-		{
-			snprintf(buffer, sizeof(buffer), "%s/%s", token, args[0]);
-			if (access(buffer, X_OK) == 0)
-			{
-				args[0] = buffer;
-				break;
-			}
-			token = strtok(NULL, ":");
-		}
-	}
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror(program_name);
-		return;
-	}
-	if (pid == 0)
-	{
-		execve(args[0], args, environ);
-		perror(program_name);
-		exit(127);
-	}
+	if (access(args[0], X_OK) == 0)
+		cmd = args[0];
 	else
 	{
-		waitpid(pid, &status, 0);
+		p = getenv("PATH");
+		if (p)
+		{
+			pcopy = strdup(p);
+			tok = strtok(pcopy, ":");
+			while (tok && !cmd)
+			{
+				snprintf(path, PATH_MAX, "%s/%s", tok, args[0]);
+				if (access(path, X_OK) == 0)
+					cmd = strdup(path);
+				tok = strtok(NULL, ":");
+			}
+			free(pcopy);
+		}
 	}
+	if (!cmd)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n", prog, args[0]);
+		return;
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(cmd, args, environ);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+		waitpid(pid, &status, 0);
+	if (cmd != args[0])
+		free(cmd);
 }
-
--- main.c
+++ main.c
@@ -1,23 +1,65 @@
 #include "shell.h"
 
 /**
+ * prompt_user - Displays prompt if shell is interactive
+ */
+void prompt_user(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+}
+
+/**
+ * handle_input - Handles user input and command execution
+ * @line: pointer to the input buffer
+ * @nread: number of characters read
+ * Return: -1 to exit shell, 0 to continue
+ */
+int handle_input(char *line, ssize_t nread)
+{
+	char **args;
+
+	if (nread > 0 && line[nread - 1] == '\n')
+		line[nread - 1] = '\0';
+
+	args = parse_line(line);
+	if (args == NULL)
+		return (0);
+
+	if (_strcmp(args[0], "exit") == 0)
+	{
+		free_args(args);
+		return (-1);
+	}
+	else if (_strcmp(args[0], "env") == 0)
+	{
+		print_env();
+	}
+	else
+	{
+		execute_command(args, "./hsh");
+	}
+	free_args(args);
+	return (0);
+}
+
+/**
  * main - Entry point for the shell
  *
- * Return: 0 on success, -1 on failure
+ * Return: 0 on success
  */
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
-	char **args;
+	int status = 0;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
-
+		prompt_user();
 		nread = getline(&line, &len, stdin);
+
 		if (nread == -1)
 		{
 			free(line);
@@ -25,28 +67,11 @@ int main(void)
 			break;
 		}
 
-		if (nread > 0 && line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
-
-		args = parse_line(line);
-		if (args == NULL)
-			continue;
-
-		if (_strcmp(args[0], "exit") == 0)
-		{
-			free_args(args);
+		status = handle_input(line, nread);
+		if (status == -1)
 			break;
-		}
-		else if (_strcmp(args[0], "env") == 0)
-		{
-			print_env();
-		}
-		else
-		{
-			execute_command(args, args[0]);
-		}
-		free_args(args);
 	}
+
 	free(line);
 	return (0);
 }
