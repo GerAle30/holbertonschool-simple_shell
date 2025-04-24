1228cd6c0613fbdf40823a3c239d34718189d14a
Gerdia0117 <Gerald.froz@gmail.com>
2025-04-24 11:09:11 -0400

    adding updates

--- main.c
+++ main.c
@@ -1,75 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "shell.h"
 
 /**
- * prompt_user - Displays prompt if shell is interactive
- */
-void prompt_user(void)
-{
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "$ ", 2);
-}
-
-/**
- * handle_input - Handles user input and command execution
- * @line: pointer to the input buffer
- * @nread: number of characters read
- * Return: -1 to exit shell, 0 to continue
- */
-int handle_input(char *line, ssize_t nread)
-{
-	char **args;
-
-	if (nread > 0 && line[nread - 1] == '\n')
-		line[nread - 1] = '\0';
-
-	args = parse_line(line);
-	if (args == NULL)
-		return (0);
-
-	if (_strcmp(args[0], "exit") == 0)
-	{
-		free_args(args);
-		return (-1);
-	}
-	else if (_strcmp(args[0], "env") == 0)
-	{
-		print_env();
-	}
-	else
-	{
-		execute_command(args, "./hsh");
-	}
-	free_args(args);
-	return (0);
-}
-
-/**
- * main - Entry point for the shell
+ * main - Entry point of the simple shell
+ * @argc: Argument count (unused)
+ * @argv: Argument vector (used for error messages)
  *
  * Return: 0 on success
  */
-int main(void)
+int main(int argc, char **argv)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
-	int status = 0;
+	char **args = NULL;
+	(void)argc;
 
 	while (1)
 	{
-		prompt_user();
-		nread = getline(&line, &len, stdin);
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, " $ ", 2);
 
+		nread = getline(&line, &len, stdin);
 		if (nread == -1)
 		{
 			free(line);
-			write(STDOUT_FILENO, "\n", 1);
-			break;
+			exit(0);
 		}
 
-		status = handle_input(line, nread);
-		if (status == -1)
-			break;
+		args = parse_line(line);
+
+		if (args == NULL || args[0] == NULL)
+		{
+			free_args(args);
+			continue;
+		}
+
+		if (handle_builtin(args, line))
+		{
+			free_args(args);
+			continue;
+		}
+
+		execute_command(args, argv[0]);
+
+		free_args(args);
 	}
 
 	free(line);
